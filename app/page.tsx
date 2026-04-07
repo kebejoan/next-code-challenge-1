8ade4bd8c0c6b79c5a252bc0370928707df75a46
kebejoan <jmailed.joe@gmail.com>
2026-04-07 21:37:36 +0700

    feat: finish the skeleton of the page

--- app/page.tsx
+++ app/page.tsx
@@ -10,33 +10,52 @@ export default async function Home({ searchParams }: pageProps) {
   // const res = await fetch("http://localhost:3000/data/dummy.json");
   // const data = await res.json();
 
-  const params = await searchParams; //why does this must be await but it hints that it does not affect
+  const params = await searchParams; //why does this must be await but it hints that "'await' has no effect on the type of this expression."
   const { category, subcategory, brand } = params;
 
   const data = CATALOG_DATA;
 
+  //brain of the filter
   const filteredData = CATALOG_DATA.products.filter((p: Product) => {
     if (brand) return p.brandId === brand;
+
+    if (subcategory) {
+      const brandsInSub = CATALOG_DATA.brands
+        .filter((b) => b.subCategoryId === subcategory)
+        .map((b) => b.id);
+      return brandsInSub.includes(p.brandId);
+    }
+
+    if (category) {
+      const subsInCat = CATALOG_DATA.subCategories
+        .filter((s) => s.categoryId === category)
+        .map((s) => s.id);
+      const brandsInCat = CATALOG_DATA.brands
+        .filter((b) => subsInCat.includes(b.subCategoryId))
+        .map((b) => b.id);
+      return brandsInCat.includes(p.brandId);
+    }
+
     return true;
   });
 
   return (
-    <main className="">
-      <section>
-        <h1>Products</h1>
-        {filteredData.map((product: Product) => (
-          <div key={product.id}>
-            <p>{product.id}</p>
-            <h2>{product.name}</h2>
-            <p>{product.brandId}</p>
-            <p>${product.price}</p>
-          </div>
-        ))}
-      </section>
-      <div>
-        <p>Category: {category}</p>
-        <p>Subcategory: {subcategory}</p>
-        <p>Brand: {brand}</p>
+    <main className="w-full flex flex-col md:flex-row ">
+      {/* Comboboxes */}
+      <div className="w-1/4 outline-1"> Comboboxes </div>
+      <div className="grow flex flex-col outline-2">
+        {/* Breadcrumbs */}
+        <nav> Breadcrumbs </nav>
+        {/* Products */}
+        <section>
+          <h1>Products</h1>
+          {filteredData.map((product: Product) => (
+            <div key={product.id}>
+              <h2>{product.name}</h2>
+              <p>{product.price}</p>
+            </div>
+          ))}
+        </section>
       </div>
     </main>
   );
